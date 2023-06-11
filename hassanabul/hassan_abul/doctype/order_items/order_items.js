c30a505d408c0c72bcd69daf082bf0358016ddf4
UsamaNaveed9 <usamanaveed9263@gmail.com>
2023-06-11 23:54:12 +0000

    fect:order items to auto create material request

--- hassanabul/hassan_abul/doctype/order_items/order_items.js
+++ hassanabul/hassan_abul/doctype/order_items/order_items.js
@@ -2,7 +2,28 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Order Items', {
-	// refresh: function(frm) {
-
-	// }
+	get_items: function(frm) {
+		frm.clear_table("items");
+		frappe.call({
+			method: "hassanabul.hassan_abul.doctype.order_items.order_items.get_all_items",
+			freeze: true,
+			freeze_message: "Processing",
+			callback: function(r) {
+					if(r.message) {
+						//console.log(r.message);
+						let items_details = r.message;
+						for (let i = 0; i < items_details.length; i++){
+							let row_item = cur_frm.add_child("items");
+							row_item.item_code = items_details[i].item_code;
+							row_item.item_name = items_details[i].item_name;
+							row_item.item_group = items_details[i].item_group;
+							row_item.qty_onhand = items_details[i].qty_onhand;
+							row_item.uom = items_details[i].stock_uom;
+							row_item.unit_cost = items_details[i].valuation_rate;
+							cur_frm.refresh_field("items");
+						}
+					}
+			}
+		});
+	}
 });
--- hassanabul/hassan_abul/doctype/order_items/order_items.py
+++ hassanabul/hassan_abul/doctype/order_items/order_items.py
@@ -1,8 +1,89 @@
 # Copyright (c) 2023, SMB and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class OrderItems(Document):
-	pass
+	def before_save(self):
+		if self.items:
+			for row in self.items:
+				if(row.processing_qty==0):
+					row.processing_qty = 1
+
+	def on_submit(self):
+		if self.items:
+			mr = frappe.new_doc("Material Request")
+			mr.material_request_type = "Purchase"
+			mr.transaction_date = self.posting_date
+			mr.schedule_date = self.posting_date
+			mr.order_items = self.name
+			
+			for row in self.items:
+				mr_item = frappe.new_doc("Material Request Item")
+				mr_item.item_code = row.item_code
+				mr_item.item_name = row.item_name
+				mr_item.schedule_date = self.posting_date
+				mr_item.qty = row.processing_qty
+				mr_item.uom = row.uom
+				mr.append("items", mr_item)
+
+			mr.save()
+			mr.submit()				
+
+
+@frappe.whitelist()
+def get_all_items():
+	items = frappe.get_all('Item', filters={'disabled': 0}, fields=['item_code', 'item_name', 'item_group', 'stock_uom', 'size_p'])
+	for row in items:
+		row['valuation_rate'] = frappe.db.get_value("Bin", {"item_code":row.item_code}, "valuation_rate")
+		row['qty_onhand'] = frappe.db.get_value("Bin", {"item_code":row.item_code}, "actual_qty")
+
+	return items
+
+
+
+
+
+
+	# main_items = []
+	# for so in sales_order:
+	# 	so_items = frappe.db.sql('''select item_code,item_name,qty,uom,rate,amount,parent from `tabSales Order Item` 
+	# 						where `tabSales Order Item`.parent = "{0}"'''.format(so),as_dict = 1)
+	# 	for soi in so_items:					
+	# 		main_items.append(soi)					
+
+	# bom = {}
+	# boms = []
+	# recipe_item = {}
+	# recipe_items = []
+
+	# for item in main_items:
+	# 	bom['bom_name'] = frappe.db.get_value("BOM", {"item": item.item_code, "is_default": 1}, "name")
+	# 	bom['total_cost'] = frappe.db.get_value("BOM", {"item": item.item_code, "is_default": 1}, "total_cost")
+	# 	bom['main_item'] = item.item_code
+	# 	bom['sales_order'] = item.parent					
+
+	# 	bom_copy = bom.copy()
+	# 	boms.append(bom_copy)
+
+	# 	if bom['bom_name']:
+	# 		bom_doc = frappe.get_doc('BOM', bom['bom_name'])
+	# 		bom_doc_items = bom_doc.items
+	# 		for re_item in bom_doc_items:
+	# 			recipe_item['item_code'] = re_item.item_code
+	# 			recipe_item['item_name'] = re_item.item_name
+	# 			recipe_item['qty'] = re_item.qty
+	# 			recipe_item['uom'] = re_item.uom
+	# 			recipe_item['available_qty'] = frappe.db.get_value("Bin", {"item_code":re_item.item_code, "warehouse":source_warehouse}, "actual_qty")
+	# 			recipe_item['rate'] = frappe.db.get_value("Bin", {"item_code":re_item.item_code, "warehouse":source_warehouse}, "valuation_rate")
+	# 			recipe_item['amount'] = recipe_item['qty'] * recipe_item['rate']
+	# 			recipe_item['parent_item'] = item.item_code
+	# 			recipe_item['bom'] = re_item.parent
+	# 			recipe_item['sales_order'] = item.parent
+
+	# 			recipe_item_copy = recipe_item.copy()
+	# 			recipe_items.append(recipe_item_copy)
+
+	
+	# return main_items,boms,recipe_items
